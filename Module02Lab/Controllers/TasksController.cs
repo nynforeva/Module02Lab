c3e6e0eeb0d9b1d6dc320ecf60df6f521248960d
Dilnaz <Dilnaz@ULTRAMACHINE>
2026-09-17 17:05:10 +0500

    Добавьте файлы проекта.

---
+++ Module02Lab/Controllers/TasksController.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Module02Lab.Models;
+
+namespace Module02Lab.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TasksController : ControllerBase
+    {
+        private static readonly List<TaskItem> tasks = new();
+
+        // GET: api/tasks
+        [HttpGet]
+        public ActionResult<IEnumerable<TaskItem>> GetAll()
+        {
+            return Ok(tasks);
+        }
+
+        // GET: api/tasks/1
+        [HttpGet("{id:int}")]
+        public ActionResult<TaskItem> GetById(int id)
+        {
+            var task = tasks.FirstOrDefault(t => t.Id == id);
+
+            if (task == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(task);
+        }
+
+        // POST: api/tasks
+        [HttpPost]
+        public ActionResult<TaskItem> Create(TaskItem task)
+        {
+            if (task.Id <= 0 || string.IsNullOrWhiteSpace(task.Title))
+            {
+                return BadRequest();
+            }
+
+            if (tasks.Any(t => t.Id == task.Id))
+            {
+                return BadRequest();
+            }
+
+            tasks.Add(task);
+
+            return CreatedAtAction(
+                nameof(GetById),
+                new { id = task.Id },
+                task
+            );
+        }
+
+        // PUT: api/tasks/1
+        [HttpPut("{id:int}")]
+        public ActionResult<TaskItem> Update(int id, TaskItem updatedTask)
+        {
+            if (id != updatedTask.Id ||
+                string.IsNullOrWhiteSpace(updatedTask.Title))
+            {
+                return BadRequest();
+            }
+
+            var task = tasks.FirstOrDefault(t => t.Id == id);
+
+            if (task == null)
+            {
+                return NotFound();
+            }
+
+            task.Title = updatedTask.Title;
+            task.Description = updatedTask.Description;
+            task.IsCompleted = updatedTask.IsCompleted;
+
+            return Ok(task);
+        }
+
+        // DELETE: api/tasks/1
+        [HttpDelete("{id:int}")]
+        public ActionResult<TaskItem> Delete(int id)
+        {
+            var task = tasks.FirstOrDefault(t => t.Id == id);
+
+            if (task == null)
+            {
+                return NotFound();
+            }
+
+            tasks.Remove(task);
+
+            return Ok(task);
+        }
+    }
+}
---
+++ Module02Lab/Models/TaskItem.cs
@@ -0,0 +1,13 @@
+﻿namespace Module02Lab.Models
+{
+    public class TaskItem
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; } = string.Empty;
+
+        public string Description { get; set; } = string.Empty;
+
+        public bool IsCompleted { get; set; }
+    }
+}
